1dde9191c14c734f10efe7f5baf9ef4305a2e594
NYehor <yehornedbalo@gmail.com>
2020-04-28 20:43:46 +0300

    Added interface

---
+++ GraphAlgorithm/IMethod.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphAlgorithm
+{
+    interface IMethod
+    {
+        string Resolve(List<List<int>> matrix, bool isIncidentMatrix);
+    }
+}
