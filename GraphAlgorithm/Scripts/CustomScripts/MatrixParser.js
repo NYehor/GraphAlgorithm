a57556915820a46a52d6b9ad1d94023fe0ab453f
NYehor <yehornedbalo@gmail.com>
2020-04-28 20:16:31 +0300

    start project

---
+++ GraphAlgorithm/Scripts/CustomScripts/MatrixParser.js
@@ -0,0 +1,14 @@
+﻿function ParseTextInput(matrix, separator) {
+
+    let text = document.getElementById(matrix).value;
+    let strArr = text.split('\n');
+
+    var outputMatrix = [strArr.length];
+
+    for (var i = 0; i < strArr.length; i++) {
+
+        outputMatrix[i] = strArr[i].trim().split(separator);
+    }
+
+    return outputMatrix;
+}
