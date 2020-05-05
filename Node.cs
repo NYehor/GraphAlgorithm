85fbd9d0894fe08355797cc6f84dfb87ed81918f
Julia Zubkova <36541428+Juliazu@users.noreply.github.com>
2020-05-06 02:34:19 +0300

    Add files via upload

---
+++ Node.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeigtedTree
+{
+    class Node<T> : IComparable
+    {
+        internal Node(double probability, T value)
+        {
+            Probability = probability;
+            LeftSon = RightSon = Parent = null;
+            Value = value;
+            IsLeaf = true;
+        }
+
+        internal Node(Node<T> leftSon, Node<T> rightSon)
+        {
+            LeftSon = leftSon;
+            RightSon = rightSon;
+            Probability = leftSon.Probability + rightSon.Probability;
+            leftSon.IsZero = true;
+            rightSon.IsZero = false;
+            leftSon.Parent = rightSon.Parent = this;
+            IsLeaf = false;
+        }
+
+        internal Node<T> LeftSon { get; set; }
+        internal Node<T> RightSon { get; set; }
+        internal Node<T> Parent { get; set; }
+        internal T Value { get; set; }
+        internal bool IsLeaf { get; set; }
+        internal  bool IsZero { get; set; }
+        internal int Bit
+        {
+            get { return IsZero ? 0 : 1; }
+        }
+
+        internal bool IsRoot
+        {
+            get { return Parent == null; }
+        }
+
+       double Probability { get; set; }
+
+        public int CompareTo(object obj)
+        {
+            return -Probability.CompareTo(((Node<T>)obj).Probability);
+        }
+    }
+}
+	
---
+++ Tree.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WeigtedTree
+{
+	class Tree<T> where T : IComparable
+    {
+        private readonly Dictionary<T, Node<T>> _leafDictionary = new Dictionary<T, Node<T>>();
+        private readonly Node<T> _root;
+
+        public Tree(IEnumerable<T> values)
+        {
+            var counts = new Dictionary<T, int>();
+            var priorityQueue = new PriorityQueue<Node<T>>();
+            int valueCount = 0;
+
+            foreach (T value in values)
+            {
+                if (!counts.ContainsKey(value))
+                {
+                    counts[value] = 0;
+                }
+                counts[value]++;
+                valueCount++;
+            }
+
+            foreach (T value in counts.Keys)
+            {
+                var node = new Node<T>((double)counts[value] / valueCount, value);
+                priorityQueue.Add(node);
+                _leafDictionary[value] = node;
+            }
+
+            while (priorityQueue.Count > 1)
+            {
+                Node<T> leftSon = priorityQueue.Pop();
+                Node<T> rightSon = priorityQueue.Pop();
+                var parent = new Node<T>(leftSon, rightSon);
+                priorityQueue.Add(parent);
+            }
+
+            _root = priorityQueue.Pop();
+            _root.IsZero = false;
+        }
+
+        public List<int> Encode(T value)
+        {
+            var returnValue = new List<int>();
+            Encode(value, returnValue);
+            return returnValue;
+        }
+
+        public void Encode(T value, List<int> encoding)
+        {
+            if (!_leafDictionary.ContainsKey(value))
+            {
+                throw new ArgumentException("Invalid value in Encode");
+            }
+            Node<T> nodeCur = _leafDictionary[value];
+            var reverseEncoding = new List<int>();
+            while (!nodeCur.IsRoot)
+            {
+                reverseEncoding.Add(nodeCur.Bit);
+                nodeCur = nodeCur.Parent;
+            }
+
+            reverseEncoding.Reverse();
+            encoding.AddRange(reverseEncoding);
+        }
+
+        public List<int> Encode(IEnumerable<T> values)
+        {
+            var returnValue = new List<int>();
+
+            foreach (T value in values)
+            {
+                Encode(value, returnValue);
+            }
+            return returnValue;
+        }
+
+        public T Decode(List<int> bitString, ref int position)
+        {
+            Node<T> nodeCur = _root;
+            while (!nodeCur.IsLeaf)
+            {
+                if (position > bitString.Count)
+                {
+                    throw new ArgumentException("Invalid bitstring in Decode");
+                }
+                nodeCur = bitString[position++] == 0 ? nodeCur.LeftSon : nodeCur.RightSon;
+            }
+            return nodeCur.Value;
+        }
+
+        public List<T> Decode(List<int> bitString)
+        {
+            int position = 0;
+            var returnValue = new List<T>();
+
+            while (position != bitString.Count)
+            {
+                returnValue.Add(Decode(bitString, ref position));
+            }
+            return returnValue;
+        }
+    }
+
+}
+
