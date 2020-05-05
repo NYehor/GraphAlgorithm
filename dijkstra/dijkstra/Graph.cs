4c11c6c384da205e74cc2c2ff770d21333e08fd9
vladkvts <vladyakovets@gmail.com>
2020-05-05 23:23:00 +0300

    add dijkstra algorithm console app

---
+++ dijkstra/dijkstra/Graph.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace dijkstra
+{
+    public class Graph
+    {
+        public List<List<double>> AdjacencyMatrix { get; set; }
+        public List<Vertice> Vertices { get; set; }
+
+        public Graph(List<List<double>> matrix)
+        {
+            AdjacencyMatrix = matrix;
+            Vertices = new List<Vertice>();
+            for (int i = 0; i < matrix.Count; i++)
+            {
+                Vertices.Add(new Vertice(i));
+            }
+            LinkNodes();
+        }
+
+        public bool AreAllNodesRemoved()
+        {
+            var allremoved = true;
+            foreach (var item in Vertices)
+            {
+                if (!item.IsRemoved)
+                {
+                    allremoved = false;
+                }
+            }
+            return allremoved;
+        }
+
+        private void LinkNodes()
+        {
+            for (int i = 0; i < Vertices.Count; i++)
+            {
+                for (int j = 0; j < Vertices.Count; j++)
+                {   
+                    if (!double.IsInfinity(AdjacencyMatrix[i][j]))
+                    {
+                        Vertices[i].Siblings.Add(Vertices[j]);
+                    }
+                }
+            }
+        }
+    }
+
+    public class Vertice
+    {
+        public int Number { get; set; }
+        public bool IsRemoved { get; set; } = false;
+        public List<Vertice> Siblings { get; set; } = new List<Vertice>();
+        public Vertice NearestVertice { get; set; } = null;
+        public double ShortestPathLength { get; set; } = double.PositiveInfinity;
+
+
+
+        public Vertice(int number)
+        {
+            Number = number;
+        }
+    }
+}
---
+++ dijkstra/dijkstra/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace dijkstra
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dijkstra = new Dijkstra();
+            dijkstra.Resolve(1);
+            Console.Read();
+        }
+    }
+}
