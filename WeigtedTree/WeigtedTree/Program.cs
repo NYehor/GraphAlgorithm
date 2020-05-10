485546e7d057c26509a05465d2ce42f9c337ab34
Yulia Zubkova <zubkova.yulia10@gmail.com>
2020-05-10 22:27:15 +0300

    Weighted Tree

---
+++ WeigtedTree/WeigtedTree/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WeigtedTree
+{
+	class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Please enter the string:");
+            string Example = Console.ReadLine();
+            var tree = new Tree<char>(Example);
+            List<int> encoding = tree.Encode(Example);
+            List<char> decoding = tree.Decode(encoding);
+            var outString = new string(decoding.ToArray());
+            Console.WriteLine(outString == Example ? "Encoding/decoding worked" : "Encoding/Decoding failed");
+
+            var chars = new HashSet<char>(Example);
+            foreach (char c in chars)
+            {
+                encoding = tree.Encode(c);
+                Console.Write("{0}:  ", c);
+                foreach (int bit in encoding)
+                {
+                    Console.Write("{0}", bit);
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+    
+	
+}
